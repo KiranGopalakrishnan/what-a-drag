dfad514c13194cacb078dddf704237fb503e1adf
Kiran Gopalakrishnan <kgopalakrishnan@tempo.io>
2019-08-12 18:18:10 -0400

    adding actions

---
+++ actions/version-major.sh
@@ -0,0 +1,7 @@
+cd build
+tag=$(npm version major 2>&1)
+tag=${tag#*@}
+git tag -a "$tag" -m "$tag"
+cd ..
+git add .
+git commit -m "$tag"
---
+++ actions/version-patch.sh
@@ -0,0 +1,7 @@
+cd build
+tag=$(npm version patch 2>&1)
+tag=${tag#*@}
+git tag -a "$tag" -m "$tag"
+cd ..
+git add .
+git commit -m "$tag"
